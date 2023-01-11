5aee0f37a64a1baddfed4f73b8e594ae69fe1552
柠凉 <1292571653@qq.com>
2023-01-11 12:09:12 +0800

    相机跟随

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Transform target;
+    public Vector2 minPos;
+    public Vector2 maxPos;
+
+    private float smoothing = 0.1f;//平滑因子
+
+    void Start()
+    {
+
+    }
+
+    void FixedUpdate()
+    {
+        if (target != null && transform.position != target.position)
+        {
+            Vector3 targetPos = target.position;
+            targetPos.x = Mathf.Clamp(targetPos.x, minPos.x, maxPos.x);//限制相机移动范围
+            targetPos.y = Mathf.Clamp(targetPos.y, minPos.y, maxPos.y);
+            transform.position = Vector3.Lerp(transform.position, targetPos, smoothing);//线性插值
+        }
+    }
+
+    // Update is called once per frame
+
+    public void SetCamPosLimit(Vector2 minPosition, Vector2 maxPosition)
+    {
+        minPos = minPosition;
+        maxPos = maxPosition;
+    }
+}
