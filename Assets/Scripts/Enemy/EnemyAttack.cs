24a10f325964e6cd443d39f1edc7cf6dbd36614f
qmqmqqm <1553984288@qq.com>
2023-01-15 12:17:29 +0800

    解决了远程小怪动画与攻击不同步，不能一直攻击的问题，解决了受击动画偶尔不出现的问题

--- Assets/Scripts/Enemy/EnemyAttack.cs
+++ Assets/Scripts/Enemy/EnemyAttack.cs
@@ -1,30 +1,56 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class EnemyAttack : MonoBehaviour
 {
-
     private Animator anim;
     private Animator attack;
-    private BoxCollider2D coll2D;
+    public float radius;//距离
     public AudioSource attackMusic;
-    // Start is called before the first frame update
+    public float time;
+    public float startTime;
+    private BoxCollider2D coll2D;
+    private Transform playertransform;//player的坐标
     void Start()
     {
+        playertransform = GameObject.FindGameObjectWithTag("player").GetComponent<Transform>();
         anim = GetComponent<Animator>();
         attack = transform.parent.GetComponent<Animator>();
         coll2D = GetComponent<BoxCollider2D>();
     }
-   
+     void Update()
+    {
+        if (playertransform != null)
+        {
+            float distance = Mathf.Abs(transform.position.x - playertransform.position.x);//玩家和敌人的距离。为一个
+            if (distance <=radius&&attack.GetBool("attack")==false)//玩家进入巡逻半径
+            {
+                attack.SetBool("attack", true);
+                anim.SetBool("attack", true);
+                StartCoroutine(StartAttack());
+                attackMusic.Play();
+            }
+        }
+    }
+    IEnumerator StartAttack()
+    {
+        yield return new WaitForSeconds(startTime);//延时
+        coll2D.enabled = true;
+        StartCoroutine(disabledHitbox());
+    }
+    IEnumerator disabledHitbox()
+    {
+        yield return new WaitForSeconds(time);//延时
+        coll2D.enabled = false;
+    }
     public void StopAttack()
     {
         attack.SetBool("attack", false);
         anim.SetBool("attack", false);
-        
     }
-    
-    void OnTriggerEnter2D(Collider2D other)
+ /*   void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.CompareTag("player"))
         {
@@ -32,6 +58,5 @@ public class EnemyAttack : MonoBehaviour
             anim.SetBool("attack", true);
             attackMusic.Play();
         }
-        
-    }
+    }*/
 }
