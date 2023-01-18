955182554da4e90f6f59f1a03d4b702a0906a55a
柠凉 <1292571653@qq.com>
2023-01-18 14:32:07 +0800

    小修改
    道具界面关闭时修复原来点击叉叉会攻击的bug

--- Assets/Scripts/prop/prop.cs
+++ Assets/Scripts/prop/prop.cs
@@ -14,97 +14,130 @@ public class prop : MonoBehaviour
     public GameObject eyeball2;
     public GameObject spine;
     public GameObject vertebra;
-    public bool canAttack = true;
+
     public void OpenDiary1()
     {
         diary1.SetActive(true);
+        GameObject.FindGameObjectWithTag("player").transform.GetChild(2).GetComponent<playerAttack>().anim.SetBool("isattacking", false);
+        GameObject.FindGameObjectWithTag("player").transform.GetChild(2).GetComponent<playerAttack>().enabled = false;
         Time.timeScale = 0f;
     }
     public void CloseDiary1()
     {
         diary1.SetActive(false);
+        StartCoroutine(Close());
         Time.timeScale = 1f;
     }
     public void OpenDiary2()
     {
         diary2.SetActive(true);
+        GameObject.FindGameObjectWithTag("player").transform.GetChild(2).GetComponent<playerAttack>().anim.SetBool("isattacking", false);
+        GameObject.FindGameObjectWithTag("player").transform.GetChild(2).GetComponent<playerAttack>().enabled = false;
         Time.timeScale = 0f;
     }
     public void CloseDiary2()
     {
         diary2.SetActive(false);
+        StartCoroutine(Close());
         Time.timeScale = 1f;
     }
     public void OpenDiary3()
     {
         diary3.SetActive(true);
+        GameObject.FindGameObjectWithTag("player").transform.GetChild(2).GetComponent<playerAttack>().anim.SetBool("isattacking", false);
+        GameObject.FindGameObjectWithTag("player").transform.GetChild(2).GetComponent<playerAttack>().enabled = false;
         Time.timeScale = 0f;
     }
     public void CloseDiary3()
     {
         diary3.SetActive(false);
+        StartCoroutine(Close());
         Time.timeScale = 1f;
     }
     public void OpenDiary4()
     {
         diary4.SetActive(true);
+        GameObject.FindGameObjectWithTag("player").transform.GetChild(2).GetComponent<playerAttack>().anim.SetBool("isattacking", false);
+        GameObject.FindGameObjectWithTag("player").transform.GetChild(2).GetComponent<playerAttack>().enabled = false;
         Time.timeScale = 0f;
     }
     public void CloseDiary4()
     {
         diary4.SetActive(false);
+        StartCoroutine(Close());
         Time.timeScale = 1f;
     }
 
     public void OpenTentacle2()
     {
         tentacle2.SetActive(true);
+        GameObject.FindGameObjectWithTag("player").transform.GetChild(2).GetComponent<playerAttack>().anim.SetBool("isattacking", false);
+        GameObject.FindGameObjectWithTag("player").transform.GetChild(2).GetComponent<playerAttack>().enabled = false;
         Time.timeScale = 0f;
     }
     public void CloseTentacle2()
     {
         tentacle2.SetActive(false);
+        StartCoroutine(Close());
         Time.timeScale = 1f;
     }
     public void OpenRing()
     {
         ring.SetActive(true);
+        GameObject.FindGameObjectWithTag("player").transform.GetChild(2).GetComponent<playerAttack>().anim.SetBool("isattacking", false);
+        GameObject.FindGameObjectWithTag("player").transform.GetChild(2).GetComponent<playerAttack>().enabled = false;
         Time.timeScale = 0f;
     }
     public void CloseRing()
     {
         ring.SetActive(false);
+        StartCoroutine(Close());
         Time.timeScale = 1f;
     }
     public void OpenEyeball2()
     {
         eyeball2.SetActive(true);
+        GameObject.FindGameObjectWithTag("player").transform.GetChild(2).GetComponent<playerAttack>().anim.SetBool("isattacking", false);
+        GameObject.FindGameObjectWithTag("player").transform.GetChild(2).GetComponent<playerAttack>().enabled = false;
         Time.timeScale = 0f;
     }
     public void CloseEyeball2()
     {
         eyeball2.SetActive(false);
+        StartCoroutine(Close());
         Time.timeScale = 1f;
     }
     public void OpenSpine()
     {
         spine.SetActive(true);
+        GameObject.FindGameObjectWithTag("player").transform.GetChild(2).GetComponent<playerAttack>().anim.SetBool("isattacking", false);
+        GameObject.FindGameObjectWithTag("player").transform.GetChild(2).GetComponent<playerAttack>().enabled = false;
         Time.timeScale = 0f;
     }
     public void CloseSpine()
     {
         spine.SetActive(false);
+        StartCoroutine(Close());
         Time.timeScale = 1f;
     }
     public void OpenVertebra()
     {
         vertebra.SetActive(true);
+        GameObject.FindGameObjectWithTag("player").transform.GetChild(2).GetComponent<playerAttack>().anim.SetBool("isattacking", false);
+        GameObject.FindGameObjectWithTag("player").transform.GetChild(2).GetComponent<playerAttack>().enabled = false;
         Time.timeScale = 0f;
     }
     public void CloseVertebra()
     {
         vertebra.SetActive(false);
+        StartCoroutine(Close());
         Time.timeScale = 1f;
+    }
+
+    IEnumerator Close()
+    {
+        yield return new WaitForSeconds(0.2f);//—” ±
+        GameObject.FindGameObjectWithTag("player").transform.GetChild(2).GetComponent<playerAttack>().enabled = true;
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
@@ -112,6 +145,11 @@ public class prop : MonoBehaviour
         {
             Destroy(gameObject);
             button.SetActive(true);
+            GameObject.FindGameObjectWithTag("player").transform.GetChild(2).GetComponent<playerAttack>().anim.SetBool("isattacking", false);
+            GameObject.FindGameObjectWithTag("player").transform.GetChild(2).GetComponent<playerAttack>().enabled = false;
+            Time.timeScale = 0f;
+            StartCoroutine(Close());
+            Time.timeScale = 1f;
         }
     }
 
